1fbec611b3e85b3c8c251dd16c508e5387466005
Sourav Goswami <gosour@gmail.com>
2020-02-11 00:10:59 +0530

    Kilo editor scrolling and status bar implemented

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Projects
 A collection of projects that I did for learning purposes
 
-#List of projects:
+# List of projects:
 1. A text editor [Working][In progress]
 2. Lookup / Key Value Store [Not started]
-3. Simple Debugger
-7. Bittorrent client
-4. Linux containers
-5. Emulation
-6. Simple raycasting
+3. Simple Debugger [Not started]
+7. Bittorrent client [Not started]
+4. Linux containers  [Not started]
+5. Emulation [Not started]
+6. Simple raycasting [Not started]
+
--- kilo/kilo.c
+++ kilo/kilo.c
@@ -5,8 +5,11 @@
 #include <termios.h>
 #include <sys/ioctl.h>
 #include <string.h>
+#include <time.h>
+#include <stdarg.h>
 
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 #define CTRL_KEY(k) ((k) & 0x1f) 
 
 enum editorKey {
@@ -23,17 +26,24 @@ enum editorKey {
 
 typedef struct erow {
 	int size;
+	int rsize; //rendered text size
 	char *chars;
+	char *render; //transformed text
 } erow;
 
 struct editorConfig {
 	int cx, cy; //cursor position in "file", and maintained by us
 		    //to "display" cursor wrt to terminal
-	int rowoff; //what row of file we are currently scrolled to (ie displayed from)
+	int rx;     //rx is x cursor cordinate in rendered text row
+	int rowoff; //start rendering from row = rowoff
+	int coloff; //start rendering from column = coloff
 	int screenrows; //number of rows in current screen
 	int screencols; //number of columns in current column
 	int numrows;
 	erow *row;
+	char *filename;
+	char statusmsg[80];
+	time_t statusmsg_time; //status msg will time out
 	struct termios orig_termios;
 } E;
 
@@ -122,7 +132,42 @@ int getWindowSize(int *rows, int *cols) {
 		return 0;
 	}
 }
+
 /** row operations **/
+
+int editorRowCxToRx(erow *row, int cx) {
+	int rx = 0;
+	int j;
+	for(j = 0; j<cx; j++) {
+		if(row->chars[j] == '\t')
+			rx += (KILO_TAB_STOP -1) - (rx % KILO_TAB_STOP); //
+		rx++;
+	}
+	return rx;
+}
+
+void editorUpdateRow(erow *row) 
+{
+	int tabs = 0;
+	int j;
+	for(j=0; j<row->size; j++)
+		if(row->chars[j] == '\t') tabs++;
+	free(row->render);
+	row->render = malloc(row->size + tabs*(KILO_TAB_STOP -1) + 1);
+	
+	int idx = 0;
+	for(j = 0; j< row->size; j++){
+		if(row->chars[j] == '\t') {
+			row->render[idx++] = ' ';
+			while(idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+		}
+		else
+			row->render[idx++] = row->chars[j];	
+	}
+	row->render[idx] = '\0';
+	row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
 	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 	
@@ -131,11 +176,17 @@ void editorAppendRow(char *s, size_t len) {
 	E.row[at].chars = malloc(len + 1);
 	memcpy(E.row[at].chars, s, len);
 	E.row[at].chars[len] = '\0';
+	E.row[at].rsize = 0;
+	E.row[at].render = NULL;
+	editorUpdateRow(&E.row[at]);
 	E.numrows++;	
 }
 
 /** file io **/
 void editorOpen(char *filename) {
+	free(E.filename);
+	E.filename = strdup(filename);
+
 	FILE *fp = fopen(filename,"r");
 	if(!fp) die("fopen");
 
@@ -176,6 +227,11 @@ void abFree(struct abuf *ab) {
 /**output**/
 
 void editorScroll() {
+	E.rx = E.cx;
+	if (E.cy < E.numrows) {
+		E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+	}
+
 	if(E.cy < E.rowoff) { //checks if cursor is above viewing window
 	//if E.rowoff == n and E.cy is n-1
 	//this means we want to display from E.rowoff n-1
@@ -187,6 +243,13 @@ void editorScroll() {
 	//this means the file will be displayed from the next line at top from E.rowoff
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+
+	if(E.cx < E.coloff) {
+		E.coloff = E.cx;
+	}
+	if(E.cx >= E.coloff + E.screencols) {
+		E.coloff = E.cx - E.screencols + 1;
+	} 
 }
 
 void editorDrawRows(struct abuf *ab) {
@@ -211,15 +274,54 @@ void editorDrawRows(struct abuf *ab) {
 			}
 		}
 		else {
-			int len = E.row[filerow].size;
+			int len = E.row[filerow].rsize - E.coloff;
+			if(len < 0) len = 0;
 			if(len > E.screencols) len = E.screencols;
-			abAppend(ab, E.row[filerow].chars, len);
+			abAppend(ab, &E.row[filerow].render[E.coloff], len);
 		}
 		abAppend(ab, "\x1b[K", 3);
-		if(y < E.screenrows - 1)
-			abAppend(ab, "\r\n", 2);
+		abAppend(ab, "\r\n", 2);
 
 	}
+}
+
+void editorDrawStatusBar(struct abuf *ab) {
+	abAppend(ab, "\x1b[7m", 4); //inverts color
+	char status[80], rstatus[80];
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines",
+			E.filename ? E.filename : "[No Name]", E.numrows);
+	if (len > E.screencols) len = E.screencols;
+	int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d", E.cy + 1, E.numrows);
+	abAppend(ab, status, len);	
+	while (len < E.screencols) {
+		if(E.screencols - len == rlen) {
+			abAppend(ab, rstatus, rlen);
+			break;
+		} else {	
+			abAppend(ab, " ", 1);
+			len++;
+		}
+	}
+	abAppend(ab, "\x1b[m", 3); //switches from inverted color
+	abAppend(ab, "\r\n", 2);
+
+}
+
+void editorDrawMessageBar(struct abuf *ab) {
+	abAppend(ab, "\x1b[K", 3);
+	int msglen = strlen(E.statusmsg);
+	if (msglen > E.screencols) msglen = E.screencols;
+	if (msglen && time(NULL) - E.statusmsg_time < 5)
+	abAppend(ab, E.statusmsg, msglen);
+}
+
+void editorSetStatusMessage(const char *fmt, ...) 
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(E.statusmsg, sizeof(E.statusmsg), fmt, ap);
+	va_end(ap);
+	E.statusmsg_time = time(NULL);
 }
 
 void editorRefreshScreen() {
@@ -229,11 +331,15 @@ void editorRefreshScreen() {
 	abAppend(&ab, "\x1b[H",3);
 
 	editorDrawRows(&ab);
+	editorDrawStatusBar(&ab);
+	editorDrawMessageBar(&ab);
+
+	//Draw cursor
 	char buf[32];
 	//(E.cy - E.rowoff) prevents cursor from going out of view
 	//If E.rowoff is 1 that is we have scrolled 1 line down and was because E.cy was moved 
 	//1 line below view, but on offsetting with E.rowoff we will always same result if E.rowoff = 0
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, E.cx + 1);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1, (E.rx - E.coloff) + 1);
 	abAppend(&ab, buf, strlen(buf));
 	abAppend(&ab, "\x1b[?25h", 6);
 
@@ -242,19 +348,42 @@ void editorRefreshScreen() {
 }
 
 void initEditor() {
-	E.cx = E.cy = 0;
+	E.cx = E.cy = E.rx = 0;
 	E.numrows = 0;
 	E.row = NULL;
 	E.rowoff = 0; 
+	E.coloff = 0;
+	E.filename = NULL;
+	E.statusmsg[0] = '\0';
+	E.statusmsg_time = 0;
+
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+	E.screenrows -= 2; //to accomodate status bar and message bar
 }
 
 /**input**/
 
 void editorMoveCursor(int key){
+	erow *row = (E.cy >= E.numrows)? NULL : &E.row[E.cy];
 	switch(key) {
-		case ARROW_LEFT : if(E.cx != 0) E.cx--; break;
-		case ARROW_RIGHT : if (E.cx != E.screencols - 1) E.cx++; break;
+		case ARROW_LEFT : {
+		if(E.cx != 0) 
+			E.cx--; 
+		else if(E.cy > 0){ //on left on column 0 go up to end of previous line
+			E.cy--;
+			E.cx = E.row[E.cy].size;
+		}
+		break;
+		}
+		case ARROW_RIGHT : {
+			if(row && E.cx < row->size) //Allow horizontal scrolling only till end of current row
+				E.cx++; 
+			else if (row && E.cx == row->size) {
+				E.cy++;
+				E.cx = 0;
+			}
+			break;
+		}
 		case ARROW_UP : if(E.cy != 0) E.cy--; break;
 		case ARROW_DOWN : {
 			if(E.cy < E.numrows)  //allows scrolling to bottom of screen but not beyond file
@@ -262,6 +391,11 @@ void editorMoveCursor(int key){
 			break;
 		}
 	}
+	//This is to correct horizontal scrolling till end of current row if current row changes
+	row = (E.cy >= E.numrows)? NULL : &E.row[E.cy];
+	int rowlen = row? row->size : 0;
+	if(E.cx > rowlen)
+		E.cx = rowlen;
 }
 
 void editorProcessKeyPress() {
@@ -276,11 +410,20 @@ void editorProcessKeyPress() {
 			E.cx = 0;
 			break;
 		case END_KEY:
-			E.cx = E.screencols - 1;
+			if(E.cy < E.numrows)
+				E.cx = E.row[E.cy].size;
 			break;
 		case PAGE_UP:
 		case PAGE_DOWN:
-			{
+			{	//Before simulating multiple scrolls
+				//set cursor to topmost or bottomost
+				if( c == PAGE_UP){
+					E.cy = E.rowoff;
+				} else if (c == PAGE_DOWN) {
+					E.cy = E.rowoff + E.screenrows - 1;
+					if(E.cy > E.numrows) E.cy = E.numrows;
+				}
+					
 				int times = E.screenrows;
 				while(times--)
 					editorMoveCursor(c == PAGE_UP? ARROW_UP: ARROW_DOWN);
@@ -301,6 +444,8 @@ int main(int argc, char *argv[])
 	initEditor();
 	if(argc >= 2)
 		editorOpen(argv[1]);
+
+	editorSetStatusMessage("HELP: Ctrl-Q = quit");
 
 	while(1){
 		editorRefreshScreen();
